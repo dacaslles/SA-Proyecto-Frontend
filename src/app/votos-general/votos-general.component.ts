4f10787d5f744291b44bfb06a9a4eaa91a218fa5
David Alberto Garcia Illescas <garcia.d52570@gt.allied.global>
2018-11-02 09:55:00 -0600

    Dashboard para Votos General agregado

--- src/app/votos-general/votos-general.component.ts
+++ src/app/votos-general/votos-general.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
+import { HttpClient } from '@angular/common/http';
+import { forEach } from '@angular/router/src/utils/collection';
+import { element } from '@angular/core/src/render3/instructions';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-votos-general',
@@ -8,28 +12,65 @@ import { Chart } from 'chart.js';
 })
 export class VotosGeneralComponent implements OnInit {
 
-  LineChart = []
+  LineChart = [];
+  result: any[];
+  arrLabels: String[];
+  arrValues: Number[];
+  total: Number;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
 
+    this.http.get('http://elecciones-sa.tk:8080/elecciones/rest/votos/resultados/').toPromise()
+    .then(
+      (data:any[]) => {
+        this.result = data;
+        console.warn(data);
+        
+        let i = 0;
+        this.arrLabels = new Array();
+        this.arrValues = new Array();
+        
+        this.result.forEach(element => {
+          this.arrLabels.push(element.nombre);
+          this.arrValues.push(element.cantidad);
+        });
+        
+        this.arrLabels.pop();
+        this.total = this.arrValues.pop();
+        /*for(i=0; i<this.result.length; i++){
+          
+          this.arrLabels.push(this.result[i].nombre);
+          this.arrValues.push(this.result[i].cantidad);
+
+        }*/
+        this.fillChart();
+        
+      });
+  }
+
+  fillChart(){
+    
     this.LineChart = new Chart('lineChart',{
       type: 'bar',
       data: {
-        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
+        //labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
+        labels: this.arrLabels,
         datasets: [{
-          label: 'Number of items sold in months',
-          data: [9,7,3,5,2,10,15,16,19,3,1,9],
+          label: 'Total de votos: ' + this.total,
+          //data: [9,7,3,5,2,10,15,16,19,3,1,9],
+          data: this.arrValues,
           fill: false,
           lineTension: 0.2,
           backgroundColor: ["red","blue","yellow","green","purple","orange","white","black","brown","gray","red","red"],
+          borderColor: "black",
           borderWidth: 1
         }]
       },
       options: {
         title:{
-          text: "Line Chart",
+          text: "Votos por partido",
           display: true
         },
         scales: {
